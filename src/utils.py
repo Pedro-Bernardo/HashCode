53ac9ba5422a7809cf2253954131fcd788164f76
Pedro Bernardo <pedro_bernardo@icloud.com>
2019-02-28 18:12:25 +0000

    Changed name for utils.py

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+
+
+def parseInput(path):
+    photos = []
+    lines = []
+
+    with open(path, 'r') as file:
+        lines = file.readlines()[1:]
+    
+    for l in lines:
+        l = l[:-1]
+        tokens = l.split(' ')
+        photos.append([tokens[0], tokens[2:]])
+
+    return photos
+
+
+def output
+
+#[['H', ['cat', 'beach']],['V', ['cat', 'old people']]]
